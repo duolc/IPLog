a2579fb413bb6279066a3f380d2b84d6fc8d99c1
Ryan <ryan.johnson.j@gmail.com>
2019-04-10 21:23:25 -0400

    Scheduler
    Added scheduler to run at midnight

--- ipCheck.js
+++ ipCheck.js
@@ -1,20 +1,27 @@
+//Dependencies
 var request = require('request');
+var schedule = require('node-schedule');
 
+//WAN IP Check Website
 var ipUrl = 'https://api.ipify.org/'
+
 //User Settings
+
 //Your IFTTT Webhook Key
-var iftttKey = '/with/key/'+'{{YOUR KEY HERE}}';
 var iftttLink = 'https://maker.ifttt.com/trigger/';
 
+//Run Everyday at Midnight
+var j = schedule.scheduleJob('0 0 23 * * *',function(){
 
 request ({
     url: ipUrl
 },  function(error, response, body) {
-    var now = Date();
+    var now = new Date();
     request.post(iftttLink+'ip_update'+iftttKey, {
         json: {
         value1: body
       }})
-            console.log(now)
+            console.log('      Time Checked: '+now)
             console.log('Your Current IP is: '+body);
     });
+});
